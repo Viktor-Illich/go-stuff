ab8d9958c6b9cf7bffc5adb44132efde9c99fc7c
Viktor-Illich <viktor.lazhevskyi@gmail.com>
2020-12-12 23:33:15 +0200

    handy generator

---
+++ concurrency/pipelines/handygenerator.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	_ = func(
+		done <-chan interface{},
+		values ...interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for true {
+				for _, v := range values {
+					select {
+					case <-done:
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	repeatFn := func(
+		done <-chan interface{},
+		fn func() interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for true {
+				select {
+				case <-done:
+				case valueStream <- fn():
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	rand := func() interface{} { return rand.Int() }
+	for num := range take(done, repeatFn(done, rand), 10) {
+		fmt.Println( num)
+	}
+}
