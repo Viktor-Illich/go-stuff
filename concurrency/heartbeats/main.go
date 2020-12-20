65c74223a470e5d24980d9c185a63d010dd8f2a7
Viktor-Illich <viktor.lazhevskyi@gmail.com>
2020-12-20 22:50:11 +0200

    heartbeats

---
+++ concurrency/heartbeats/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	doWork := func(
+		done <-chan interface{},
+		pulseInterval time.Duration,
+	) (<-chan interface{}, <-chan time.Time) {
+		heartbeat := make(chan interface{})
+		result := make(chan time.Time)
+		go func() {
+			pulse := time.Tick(pulseInterval)
+			workGen := time.Tick(2 * pulseInterval)
+			sendPulse := func() {
+				select {
+				case heartbeat <- struct{}{}:
+				default:
+				}
+			}
+
+			sendResult := func(r time.Time) {
+				for true {
+					select {
+					case <-pulse:
+						sendPulse()
+					case result <- r:
+						return
+					}
+				}
+			}
+
+			for i := 0; i < 2; i++ {
+				select {
+				case <-done:
+					return
+				case <-pulse:
+					sendPulse()
+				case r := <-workGen:
+					sendResult(r)
+				}
+			}
+		}()
+
+		return heartbeat, result
+	}
+
+	done := make(chan interface{})
+	time.AfterFunc(10*time.Second, func() { close(done) })
+	const timeout = 2 * time.Second
+	heartbeat, result := doWork(done, timeout/2)
+	for {
+		select {
+		case _, ok := <-heartbeat:
+			if ok == false {
+				return
+			}
+			fmt.Println("pulse")
+		case r, ok := <-result:
+			if ok == false {
+				return
+			}
+			fmt.Printf("result %v\n", r)
+		case <-time.After(timeout):
+			fmt.Println("worker goroutine is not healthy!")
+			return
+		}
+	}
+}
